645ce48e9b17c5052acaf535e9badae234dbcca5
Bobr <bobrnikita1@gmail.com>
2025-07-30 16:50:57 +0300

    заготовка пред показа сообщения с цветом (надо будет переписать на полную замену его в превью)

---
+++ lib/widgets/prewiew_message_bubble.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../models/app_settings.dart';
+import 'package:provider/provider.dart';
+
+class PreviewMessageBubble extends StatelessWidget {
+  final String text;
+  final bool isMe;
+  final Color color;
+
+  const PreviewMessageBubble({super.key, required this.text, required this.isMe, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    final settings = Provider.of<AppSettings>(context);
+    final alignment = isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start;
+    var bgColor = isMe ? color : settings.otherMessageColor;
+
+
+    return Column(
+      crossAxisAlignment: alignment,
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+              fontSize: 16 * settings.messageTextSize,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
